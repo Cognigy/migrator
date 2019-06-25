fe0bf41bccd158ecc1011353c3cb62619e7568ed
Philipp Heltewig <p.heltewig@cognigy.com>
2019-06-25 14:54:48 +0200

    encodeuricomponent

--- src/helper/MongoConnection.ts
+++ src/helper/MongoConnection.ts
@@ -3,7 +3,7 @@ import { logGreen, logRed } from './logger';
 
 export async function connectToMongo(): Promise<MongoClient> {
     // Connection URL
-    const url = `mongodb://${process.env.SOURCE_DB_USERNAME}:${process.env.SOURCE_DB_PASSWORD}@${process.env.SOURCE_DB_HOST}:${process.env.SOURCE_DB_PORT}`;
+    const url = `mongodb://${process.env.SOURCE_DB_USERNAME}:${encodeURIComponent(process.env.SOURCE_DB_PASSWORD)}@${process.env.SOURCE_DB_HOST}:${process.env.SOURCE_DB_PORT}`;
 
     return MongoClient.connect(url, { useNewUrlParser: true });
 }
